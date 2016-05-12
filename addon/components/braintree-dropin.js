815446a21afd213e504777de36c4c7b9b18838ed
James <james@aehr.org>
2016-05-12 10:45:56 -0600

    Add additional setup to braintree component

--- addon/components/braintree-dropin.js
+++ addon/components/braintree-dropin.js
@@ -5,15 +5,16 @@ import Em from 'ember';
 export default Em.Component.extend({
   action: 'processBraintreeNonce',
   token: null,
+  braintreeOptions: {},
 
   _setup: function() {
     var handler = Em.run.bind(this, this._handler),
         token = this.get('token');
 
-    braintree.setup(token, 'dropin', {
+    braintree.setup(token, 'dropin', Em.$.extend({
       container: this.elementId,
       paymentMethodNonceReceived: handler
-    });
+    }, this.get('braintreeOptions')));
   }.on('didInsertElement'),
 
   _handler: function(event, nonce) {
